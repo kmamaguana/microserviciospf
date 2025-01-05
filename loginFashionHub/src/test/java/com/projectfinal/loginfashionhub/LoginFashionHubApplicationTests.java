ecac5b6bdcfc9405b38db3cb0281e783fd06c61d
kmamaguana <kmamaguana@uce.edu.ec>
2025-01-04 23:55:19 -0500

    feat(login): ✨ We start the user authentication microservice

---
+++ loginFashionHub/src/test/java/com/projectfinal/loginfashionhub/LoginFashionHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectfinal.loginfashionhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginFashionHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

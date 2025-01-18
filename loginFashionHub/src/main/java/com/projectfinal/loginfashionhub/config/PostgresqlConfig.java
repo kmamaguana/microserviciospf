cf497502211bc5ffa4d3bd9f63c2c272dba2b07a
millanda29 <maikolllanda@gmail.com>
2025-01-18 08:09:08 -0500

    feat(config): ⚙️ Add initial configuration files for application setup
    - Added `ApplicationConfig.java` for managing authentication and password encoding.
    - Added `PostgresqlConfig.java` for initializing database tables and default admin user.
    - Added `SecurityConfig.java` to configure security settings, JWT filter, and CORS.

---
+++ loginFashionHub/src/main/java/com/projectfinal/loginfashionhub/config/PostgresqlConfig.java
@@ -0,0 +1,43 @@
+package com.projectfinal.loginfashionhub.config;
+
+import com.projectfinal.loginfashionhub.model.Role;
+import com.projectfinal.loginfashionhub.model.User;
+import com.projectfinal.loginfashionhub.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+@Configuration
+public class PostgresqlConfig {
+
+    @Bean
+    CommandLineRunner initPostgresTables(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            final String defaultUsername = "admin";
+            if (userRepository.findByUsername(defaultUsername).isPresent()) {
+                System.out.println("Administrator already exists.");
+                return;
+            }
+
+            User adminUser = createAdminUser(passwordEncoder);
+            userRepository.save(adminUser);
+            System.out.println("Administrator created successfully.");
+        };
+    }
+
+    private User createAdminUser(PasswordEncoder passwordEncoder) {
+        return User.builder()
+                .nombre("Administrador")
+                .apellido("Administrador")
+                .email("admin@facso.com")
+                .username("admin")
+                .password(passwordEncoder.encode("Admin123@"))
+                .role(Role.VENDOR)
+                .accountLocked(false)
+                .enabled(true)
+                .build();
+    }
+}

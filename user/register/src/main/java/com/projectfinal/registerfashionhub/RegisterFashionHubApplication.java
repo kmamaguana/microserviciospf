66270ab2fd90f5f30f6dc220b0930dd22599ba2a
millanda29 <maikolllanda@gmail.com>
2025-01-24 09:45:57 -0500

    feat(register): 🎉 Initial project setup

---
+++ user/register/src/main/java/com/projectfinal/registerfashionhub/RegisterFashionHubApplication.java
@@ -0,0 +1,13 @@
+package com.projectfinal.registerfashionhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterFashionHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterFashionHubApplication.class, args);
+    }
+
+}

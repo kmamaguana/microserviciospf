9a45fe5e2343e460d554d8a701c89d1884f96993
millanda29 <maikolllanda@gmail.com>
2025-01-24 08:57:10 -0500

    feat(auth): 🔥 Remove registration method in AuthService, keeping only login

--- loginFashionHub/src/main/java/com/projectfinal/loginfashionhub/service/AuthService.java
+++ loginFashionHub/src/main/java/com/projectfinal/loginfashionhub/service/AuthService.java
@@ -1,14 +1,11 @@
 package com.projectfinal.loginfashionhub.service;
 
 import com.projectfinal.loginfashionhub.request.LoginRequest;
-import com.projectfinal.loginfashionhub.request.RegisterRequest;
 import com.projectfinal.loginfashionhub.response.AuthResponse;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 
 public interface AuthService {
 
     AuthResponse login(LoginRequest request) throws AuthenticationException;
 
-    ResponseEntity<AuthResponse> register(RegisterRequest request);
 }

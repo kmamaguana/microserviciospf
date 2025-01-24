cbf2a826ed1731ad909732f7abb068d3240e6761
millanda29 <maikolllanda@gmail.com>
2025-01-24 10:03:30 -0500

    feat(dto): 📨 Create RegisterRequest and AuthResponse DTOs for handling registration and authentication data

---
+++ user/register/src/main/java/com/projectfinal/registerfashionhub/response/AuthResponse.java
@@ -0,0 +1,17 @@
+package com.projectfinal.registerfashionhub.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO for encapsulating the authentication response.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String token;
+}

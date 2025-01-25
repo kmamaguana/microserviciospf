5c52ccb7b312dd7542ede3c2a9b51b68b6e1b1e0
millanda29 <maikolllanda@gmail.com>
2025-01-25 00:50:46 -0500

    feat(passwordForgot): 🎉 Initial project setup

---
+++ user/passwordForgot/src/main/java/com/projectfinal/passwordforgotfashionhub/PasswordForgotFashionHubApplication.java
@@ -0,0 +1,13 @@
+package com.projectfinal.passwordforgotfashionhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordForgotFashionHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordForgotFashionHubApplication.class, args);
+    }
+
+}

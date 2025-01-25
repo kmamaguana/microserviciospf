37a11845e1ce0fb64d4eabf79ec855678003fb79
millanda29 <maikolllanda@gmail.com>
2025-01-24 23:59:46 -0500

    feat(passwordReset): 🎉 Initial project setup

---
+++ user/passwordReset/src/main/java/com/projectfinal/passwordresetfashionhub/PasswordResetFashionHubApplication.java
@@ -0,0 +1,13 @@
+package com.projectfinal.passwordresetfashionhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordResetFashionHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordResetFashionHubApplication.class, args);
+    }
+
+}

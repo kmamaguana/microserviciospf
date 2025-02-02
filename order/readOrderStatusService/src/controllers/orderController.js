ad698241f14aa9517979621594ae4c2da48eeb45
millanda29 <maikolllanda@gmail.com>
2025-02-02 12:10:28 -0500

    feat: 🧑‍💻 Add src/controllers/orderController.js for order handling

--- order/readOrderStatusService/src/controllers/orderController.js
+++ order/readOrderStatusService/src/controllers/orderController.js
@@ -1,16 +1,17 @@
 const orderService = require('../services/orderService');
 
-const getOrderStatus = async (req, res) => {
+const getOrderStatus = async (req, res, next) => {
+  const { id } = req.params;
+  
   try {
-    const orderId = req.params.id;
-    const orderStatus = await orderService.getOrderStatus(orderId);
+    const orderStatus = await orderService.getOrderStatus(id);
     if (orderStatus) {
       res.status(200).json({ status: orderStatus });
     } else {
       res.status(404).json({ message: 'Order not found' });
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 

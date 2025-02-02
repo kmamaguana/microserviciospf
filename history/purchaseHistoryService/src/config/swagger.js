f87cf897d6d7809383807b931ffa059b8c04a21e
millanda29 <maikolllanda@gmail.com>
2025-02-02 15:23:31 -0500

    feat: 📄 Add Swagger configuration for API documentation

--- history/purchaseHistoryService/src/config/swagger.js
+++ history/purchaseHistoryService/src/config/swagger.js
@@ -1,7 +1,7 @@
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
-const options = {
+const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
     info: {
@@ -9,17 +9,12 @@ const options = {
       version: '1.0.0',
       description: 'API to retrieve purchase history',
     },
-    servers: [
-      {
-        url: 'http://localhost:5000',
-        description: 'Local server',
-      },
-    ],
+    servers: [{ url: 'http://localhost:5000', description: 'Local server' }],
   },
-  apis: ['./src/routes/*.js'], // Ruta donde se documentarán las rutas
+  apis: ['./src/routes/*.js'],
 };
 
-const swaggerSpec = swaggerJSDoc(options);
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 const setupSwagger = (app) => {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
